dee63746475a323161f6216c710984958cb28ccf
FabriGinseng <antoniofabriziofiume@libero.it>
2018-06-20 16:41:23 +0200

    sommo

--- VoiceRecognizer/VoiceRecognizer/MainPage.xaml.cs
+++ VoiceRecognizer/VoiceRecognizer/MainPage.xaml.cs
@@ -14,14 +14,60 @@ namespace VoiceRecognizer
 		public MainPage()
 		{
 			InitializeComponent();
-		}
+            IngressoPagina();
+        }
 
-	    private async void IniziaRiconoscimento(object sender, EventArgs e)
+	    public async void IngressoPagina()
 	    {
-	       DependencyService.Get<ITextToSpeech>().Speak("INIZIO RICONOSCIMENTO VOCALE");
+	        await letturainiziale("Inserisci il valore della temperatura corporea del paziente in gradi celsius dopo il segnale acustico");
+            Device.StartTimer(TimeSpan.FromSeconds(7), () =>
 
+            {
+                InizioRiconoscimentoVocaleFebbre();
+                return false;
+	        });
+          
+        }
+
+	    public async void InserimentoPressione()
+	    {
+	        await letturainiziale("Inserisci il valore della pressione sanguigna del paziente dopo il segnale acustico");
+	        Device.StartTimer(TimeSpan.FromSeconds(7), () =>
+
+	        {
+	            InizioRiconoscimentoVocaleFebbre();
+	            return false;
+	        });
+        }
+
+	    public async Task lettura(string valore)
+	    {
+	        DependencyService.Get<ITextToSpeech>().Speak(valore);
+
+        }
+	    public async Task letturainiziale(string valorePronuncia)
+	    {
+	        DependencyService.Get<ITextToSpeech>().Speak(valorePronuncia);
+
+	    }
+
+        public async void InizioRiconoscimentoVocaleFebbre()
+	    {
 	        var result = await CrossSpeechToText.StartVoiceInput("Voice Input!");
 	        txtResult.Text = result;
+	        await lettura("hai inserito" + txtResult.Text);
+	        Device.StartTimer(TimeSpan.FromSeconds(3), () =>
+
+	        {
+	            InserimentoPressione();
+                return false;
+	        });
+
+        }
+
+        private async void IniziaRiconoscimento(object sender, EventArgs e)
+	    {
+	        
 	    }
     }
 }
